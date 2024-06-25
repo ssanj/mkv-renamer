2a4b7819e88b83e481ab9689520b9dc155b906e1
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2024-06-26 06:50:13 +1000

    add orange colour ouput for cancellations

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,16 @@ mod workflow;
 
 use cli::get_cli_args;
 use console::style;
+use models::Output;
 use workflow::perform_workflow;
 
 #[tokio::main]
 async fn main() {
   let config = get_cli_args();
+
   match perform_workflow(config).await {
-    Ok(_) => println!("{}", style("Renaming completed successfully").green()),
+    Ok(Output::Success) => println!("{}", style("Renaming completed successfully").green()),
+    Ok(Output::UserCanceled) => println!("{}", style("User canceled rename").color256(173)),
     Err(e) => eprintln!("{}", style(e)),
   }
 }
