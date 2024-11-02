2a001b358480bc347bc51c95c955835d8d22006b
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2024-11-03 09:24:35 +1100

    add notes

--- src/workflow/series/export_workflow.rs
+++ src/workflow/series/export_workflow.rs
@@ -10,8 +10,11 @@ pub async fn perform(export_args: ExportArgs) -> ROutput {
   handle_url_metadata_export(&url, export_path).await
 }
 
+// TODO: Parameterise on metadata type
 async fn handle_url_metadata_export(url: &str, export_path: PathBuf) -> ROutput {
   let page_content = download_metadata(url).await?;
+
+  // TODO: Change for movie
   let episodes_definition = get_series_metadata(&page_content);
   use std::fs::OpenOptions;
 
@@ -20,11 +23,14 @@ async fn handle_url_metadata_export(url: &str, export_path: PathBuf) -> ROutput 
     .create_new(true)
     .open(&export_path)
     .map_err(|e| {
+      // TODO: Change for movie
       RenamerError::CouldNotExportEpisodeMetadata(url.to_owned(), export_path.clone(), e.to_string())
     })
     .and_then(|file| {
+      // TODO: Change for movie - parameterise
       serde_json::to_writer_pretty(file, &episodes_definition)
         .map_err(|e| {
+          // TODO: Change for movie
           RenamerError::CouldNotExportEpisodeMetadata(url.to_owned(), export_path.clone(), e.to_string())
         })
     })
